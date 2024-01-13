9efebf03b6296fe39288a8aba89cf05d8909eed1
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-01-13 12:56:07 +0800

    handle create data for API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Table from "../components/Table";
 import axios from "axios";
+import Modal from "../components/Modal";
 
 const Home = () => {
     const [mahasiswaData, setMahasiswaData] = useState([]);
+    const [formData, setFormData] = useState({
+        // inisialisasi objek formData sesuai dengan kebutuhan Anda
+        name: "",
+        // tambahkan properti lain sesuai kebutuhan
+      });
 
     const fetchData = async () => {
         try {
@@ -18,9 +24,42 @@ const Home = () => {
         fetchData();
         const intervalId = setInterval(fetchData, 5000);
         return () => clearInterval(intervalId);
-    }, []); 
+    }, []);
 
-    return <Table data={mahasiswaData} />;
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:3000/api/v1/mahasiswa/create" , formData);
+            document.getElementById("formTambah").reset();
+
+            // Tutup modal dan ambil data terkini
+            document.getElementById("my_modal_1").close();
+            fetchData();
+        } catch (error) {
+            console.log(error)
+        };
+    };
+
+
+
+    return (
+        <>
+            <div className="container mx-auto">
+                <button className="btn btn-primary mt-2 flex ml-auto" onClick={() => document.getElementById('my_modal_1').showModal()}>Tambah Data</button>
+                <Table data={mahasiswaData} />
+                <Modal handleChange={handleChange} handleSubmit={handleSubmit} />
+            </div>
+        </>
+    );
+
+
 };
 
 export default Home;
